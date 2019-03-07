b96f902c01bc8243a2c8d156678598869461255c
Alex Coppock <acoppock@gmail.com>
2019-03-06 16:59:24 -0800

    outline

---
+++ scripts/internal_use_required_packages_installer.R
@@ -0,0 +1,6 @@
+library(checkpoint)
+
+found_packages <- scanForPackages(".", use.knitr = TRUE)$pkgs
+if (length(found_packages[!found_packages %in% installed.packages()]) > 0) {
+  install.packages(found_packages[!found_packages %in% installed.packages()])
+}
