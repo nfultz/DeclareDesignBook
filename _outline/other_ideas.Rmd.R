84be7e7bcb8240c16aa6ceb5e428a29aac040e2c
Graeme Blair <graemeblair@users.noreply.github.com>
2019-09-02 15:36:57 -0400

    outline

---
+++ _outline/other_ideas.Rmd.R
@@ -0,0 +1,34 @@
+- x Topic analysis [GB]
+- x Respondent driven sampling 
+- x Forecasting (predicting in future) [JC]
+
+- x Parallel design for mediation effects [GB]
+- x Crossover designs
+- x Selective trials [JC]
+- x Adaptive trials [AC]
+- x Multi-site designs (could be descriptive too; hard part is I) [GB]
+- x Experiments where blocks and clusters are also sampled from populations of blocks/clusters [JC]
+
+- x Cross national time series [JC]
+- x Mediation [GB]
+- x Nested designs [MH]
+- x Qualitative comparative analysis [MH]
+- x Meta-analysis [GB] 
+
+# Designs brainstorm
+
+- geographic RD
+- spatial processes
+- IRT
+- country-year FE
+- EITM
+- computational evolutional models
+- mapping social networks
+- an ANES study
+- interrupted time series
+- time series (uninterrupted)
+- selection on observables
+- "is this a good predictor"
+- need to but the handbook of lab experiments
+- weighting to get PATEs and SATEs under non-compliance and non-response when you have a baseline
+
