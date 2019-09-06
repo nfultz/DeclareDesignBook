481a183d22ebd1bf5692effe9c372a29294b074a
Alexander Coppock <acoppock@gmail.com>
2019-09-06 16:09:38 -0400

    some MIDA stuff

--- scripts/internal_use_required_packages_installer.R
+++ scripts/internal_use_required_packages_installer.R
@@ -4,3 +4,6 @@ found_packages <- scanForPackages(".", use.knitr = TRUE)$pkgs
 if (length(found_packages[!found_packages %in% installed.packages()]) > 0) {
   install.packages(found_packages[!found_packages %in% installed.packages()])
 }
+
+
+# devtools::install_github("ebenmichael/augsynth")
